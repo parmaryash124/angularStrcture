7644570badf0c82d78f94dba9d13ab4c207db524
yash.parmar <yash.parmar@agileinfoways.com>
2023-01-06 19:01:36 +0530

    template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+// it not component on every mount.// but reintilalzing the whole compoents.
+// style app.componets are effective
 export class AppComponent {
-  title = 'demoApp';
+  title = 'test';
+  user: User = {
+    username: '',
+    email: '',
+    mobileNumber: '',
+    nationality: '',
+  };
+
+  submit() {
+    console.log(this.user, 'user');
+  }
+}
+export interface User {
+  username: string;
+  email: string;
+  mobileNumber: string;
+  nationality: string;
 }
